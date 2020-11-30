5fa10f4b41b1b13e7be01dfa8d1e098288160e4c
Xicheng Wang <xw1097@gmail.com>
2020-11-29 16:30:10 -0800

    Initial Commit

---
+++ Memes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Memes
+//
+//  Created by Xicheng Wang on 11/29/20.
+//  Copyright © 2020 xichengw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
