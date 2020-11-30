0f006905f770629dad9dab08a4896aeb8363dc48
Xicheng Wang <xw1097@gmail.com>
2020-11-30 00:47:37 -0800

    keyboard sliding up & down is done, but text wont center, rotating will flip position of bottom & up

--- Memes/ViewController.swift
+++ Memes/ViewController.swift
@@ -8,15 +8,23 @@
 
 import UIKit
 
+struct Meme {
+    let topText: String
+    let bottomText: String
+    let originalImage: UIImage
+    let memedImage: UIImage
+}
+
 class ViewController: UIViewController,
-UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextViewDelegate {
+UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
     private var imagePicker: UIImagePickerController;
+    private var currentEditingTextField: UITextField?; // used to avoid unnecessary frame slide up
     
     @IBOutlet weak var shareButton: UIBarButtonItem!;
     @IBOutlet weak var cancelButton: UIBarButtonItem!;
     @IBOutlet weak var imageView: UIImageView!;
-    @IBOutlet weak var topText: UITextView!
-    @IBOutlet weak var bottomText: UITextView!
+    @IBOutlet weak var topText: UITextField!
+    @IBOutlet weak var bottomText: UITextField!
     @IBOutlet weak var cameraButton: UIBarButtonItem!;
     @IBOutlet weak var albumButton: UIBarButtonItem!;
     
@@ -25,19 +33,38 @@ UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextViewDeleg
         // empty
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad();
         // setup
         imageView.backgroundColor = .black;
         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera);
-        topText.isEditable = true;
-        bottomText.isEditable = true;
-        topText.textContainer.maximumNumberOfLines = 1;
-        bottomText.textContainer.maximumNumberOfLines = 1;
+        topText.text = "TOP";
+        bottomText.text = "BOTTOM";
+//      topText.textAlignment = .center;
+//      bottomText.textAlignment = .center;
+        topText.delegate = self;
+        bottomText.delegate = self;
+        let memeTextAttributes: [NSAttributedString.Key: Any] = [
+            NSAttributedString.Key.strokeColor: UIColor.black,
+            NSAttributedString.Key.foregroundColor: UIColor.white,
+        ];
         topText.textAlignment = .center;
         bottomText.textAlignment = .center;
-        topText.delegate = self as UITextViewDelegate;
-        bottomText.delegate = self as UITextViewDelegate;
+        topText.defaultTextAttributes = memeTextAttributes;
+        bottomText.defaultTextAttributes = memeTextAttributes;
+        shareButton.isEnabled = false;
     }
     
     @IBAction func openCamera(_ sender: Any) {
@@ -46,6 +73,22 @@ UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextViewDeleg
     
     @IBAction func openAlbum(_ sender: Any) {
         presentImagePicker(withSource: .photoLibrary)
+    }
+    
+    @IBAction func share(_ sender: UIBarButtonItem) {
+        let generatedImage = self.generateMemedImage();
+        let activity = UIActivityViewController(
+            activityItems: ["I created this picture, look!", generatedImage],
+            applicationActivities: nil
+        )
+        activity.popoverPresentationController?.barButtonItem = sender;
+        activity.completionWithItemsHandler = { (activityType: UIActivity.ActivityType?, completed:
+            Bool, arrayReturnedItems: [Any]?, error: Error?) in
+            if completed {
+                self.save(generatedImage);
+            }
+        }
+        self.present(activity, animated: true, completion: nil);
     }
     
     func presentImagePicker(withSource source: UIImagePickerController.SourceType) {
@@ -63,6 +106,7 @@ extension ViewController {
             return;
         }
         self.imageView?.image = selectedimage;
+        shareButton.isEnabled = true;
         self.imagePicker.dismiss(animated: true, completion: nil);
     }
     
@@ -72,14 +116,62 @@ extension ViewController {
 }
 
 extension ViewController {
-    func textViewDidBeginEditing(_ textView: UITextView) {
-        <#code#>
-    }(_ textField: UITextField) {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        currentEditingTextField = textField;
         textField.text = "";
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        currentEditingTextField = nil;
         textField.resignFirstResponder();
         return true;
+    }
+}
+
+// keyboard related code
+extension ViewController {
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil);
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(_ notification:Notification) {
+        if (self.bottomText == currentEditingTextField) {
+            view.frame.origin.y -= getKeyboardHeight(notification);
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification:Notification) {
+        if (view.frame.origin.y < 0) { // y is offscreen, we should retore y
+            view.frame.origin.y += getKeyboardHeight(notification);
+        }
+    }
+    
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
+    }
+}
+
+extension ViewController {
+    func save(_ memedImage: UIImage) {
+        // Create the meme
+        _ = Meme(topText: self.topText.text!, bottomText: self.bottomText.text!, originalImage: imageView.image!, memedImage: memedImage);
+    }
+    
+    func generateMemedImage() -> UIImage {
+        
+        // Render view to an image
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+        let memedImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        return memedImage
     }
 }
